5bc9d6df11ef4b732df98514c70b695a851bee6d
CHmoonKa <wen.wancai@gmail.com>
2014-11-23 23:09:59 +0800

    Update DivideTwoIntegers.java

--- src/com/iwendy/leetcode/DivideTwoIntegers.java
+++ src/com/iwendy/leetcode/DivideTwoIntegers.java
@@ -10,31 +10,24 @@ public class DivideTwoIntegers {
 	// 3. 除数向左移位(×2)然后与被除数比较，直到发现仅次于被除数的那个值，减去该值后继续。
 	// 4. 如果可以用乘的话，二分搜索倒是不错的解法。
 	public int divide(int dividend, int divisor) {
-		if(divisor == 0){
-			return Integer.MAX_VALUE;
-		}
-        long dvd = Math.abs(dividend);
-        long dvs = Math.abs(divisor);
-        
-        int sign = dividend > 0 ? 1 : -1;
-        sign = divisor > 0 ? sign : -sign;
-        int res = 0;
-        
-        int times = 0;
-        while(dvs < dvd){
-        	dvs = dvs << 1;
-        	times++;
-        }
-        
-        while(times >= 0){
-        	if(dvd >= dvs){
-        		dvd = dvd - dvs;
-        		res = res + 1<<times;
-        	}
-        	dvs >>= 1;
-        	times--;
-        }
-        
-        return res*sign;
+		if(divisor==0)
+	        return Integer.MAX_VALUE;
+	    
+	        int res = 0;
+	        boolean isNeg = ((dividend^divisor)>>>31==1)?true:false;
+	        long a = Math.abs((long)dividend);  
+	        long b = Math.abs((long)divisor); 
+	        
+	        // 以下的c有可能左移很大，则a不会减c，所以需要while(a >= b)
+	        while(a >= b){
+	            long c = b;  
+	            for(int i = 0; a >= c; i++, c <<=1)  
+	            {  
+	                a -= c;  
+	                res += 1<<i;  
+	            } 
+	        }
+	        
+	        return isNeg ? -res : res;
     }
 }
